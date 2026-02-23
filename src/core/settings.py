a44adc64951f97c6aca9843582b53f1b0752576a
xolonalibto66038 <xolonalibto66038@gmail.com>
2026-02-23 13:15:37 +0000

    change db config for prod

--- src/core/settings.py
+++ src/core/settings.py
@@ -114,7 +114,7 @@ else:
     DATABASES = {
         'default': dj_database_url.config(
             # Replace this value with your local database's connection string.
-            default='postgresql://postgres:postgres@localhost:5432/project0db',
+            default=os.environ.get("DATABASE_URL"),
             conn_max_age=600
         )
     }
